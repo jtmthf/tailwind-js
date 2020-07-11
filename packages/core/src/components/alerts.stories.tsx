f1c40bbc93c7f589aee0eb28adaa1b9c03100867
Jack Moore <jtmthf@mst.edu>
2020-07-10 22:10:52 -0500

    alert components

--- packages/core/src/components/alerts.stories.tsx
+++ packages/core/src/components/alerts.stories.tsx
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 
 import { jsx } from '@emotion/react';
-import { bgRed, bgIndigo, bgOrange } from '../backgrounds';
+import { bgRed, bgIndigo, bgOrange, bgBlue, bgTeal } from '../backgrounds';
 import {
   borderRed,
   borderWidth,
@@ -12,6 +12,9 @@ import {
   roundedT,
   borderT,
   roundedB,
+  borderTeal,
+  borderB,
+  borderBlue,
 } from '../borders';
 import { sm, lg } from '../core';
 import {
@@ -40,9 +43,12 @@ import {
   textLeft,
   textOrange,
   textWhite,
+  textSm,
+  textTeal,
+  textBlue,
 } from '../typography';
 import { itemsCenter, flexAuto } from '../flexbox';
-import { opacity } from '../effects';
+import { opacity, shadow } from '../effects';
 
 export default {
   title: 'Components/Alerts',
@@ -145,5 +151,75 @@ export const Titled = () => (
     >
       <p>Something not ideal might be happening.</p>
     </div>
+  </div>
+);
+
+export const Solid = () => (
+  <div
+    css={[
+      flex,
+      itemsCenter,
+      bgBlue(500),
+      textWhite,
+      textSm,
+      fontBold,
+      px(4),
+      py(3),
+    ]}
+    role="alert"
+  >
+    <svg css={[fillCurrent, w(4), h(4), mr(2)]} viewBox="0 0 20 20">
+      <path d="M12.432 0c1.34 0 2.01.912 2.01 1.957 0 1.305-1.164 2.512-2.679 2.512-1.269 0-2.009-.75-1.974-1.99C9.789 1.436 10.67 0 12.432 0zM8.309 20c-1.058 0-1.833-.652-1.093-3.524l1.214-5.092c.211-.814.246-1.141 0-1.141-.317 0-1.689.562-2.502 1.117l-.528-.88c2.572-2.186 5.531-3.467 6.801-3.467 1.057 0 1.233 1.273.705 3.23l-1.391 5.352c-.246.945-.141 1.271.106 1.271.317 0 1.357-.392 2.379-1.207l.6.814C12.098 19.02 9.365 20 8.309 20z" />
+    </svg>
+    <p>Something happened that you should know about.</p>
+  </div>
+);
+
+export const TopAccentBorder = () => (
+  <div
+    css={[
+      bgTeal(100),
+      borderT(4),
+      borderTeal(500),
+      roundedB(),
+      textTeal(900),
+      px(4),
+      py(3),
+      shadow('md'),
+    ]}
+    role="alert"
+  >
+    <div css={flex}>
+      <div css={py(1)}>
+        <svg
+          css={[fillCurrent, h(6), w(6), textTeal(500), mr(4)]}
+          viewBox="0 0 20 20"
+        >
+          <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z" />
+        </svg>
+      </div>
+      <div>
+        <p css={fontBold}>Our privacy policy has changed</p>
+        <p css={textSm}>Make sure you know how these changes affect you.</p>
+      </div>
+    </div>
+  </div>
+);
+
+export const Banner = () => (
+  <div
+    css={[
+      bgBlue(100),
+      borderT(),
+      borderB(),
+      borderBlue(500),
+      textBlue(700),
+      px(4),
+      py(3),
+    ]}
+    role="alert"
+  >
+    <p css={fontBold}>Informational message</p>
+    <p css={textSm}>Some additional text to explain said message.</p>
   </div>
 );
